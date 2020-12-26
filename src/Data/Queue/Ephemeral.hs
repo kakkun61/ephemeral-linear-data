fd2f936ffea1a8d98445947dea460234217286b1
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2020-12-26 19:56:07 +0900

    f → k

--- src/Data/Queue/Ephemeral.hs
+++ src/Data/Queue/Ephemeral.hs
@@ -23,17 +23,17 @@ instance Consumable a => Consumable (Queue a) where
   consume (Queue l m) = l `lseq` m `lseq` ()
 
 empty :: (Queue a #-> b) #-> b
-empty f = f (Queue [] [])
+empty k = k (Queue [] [])
 
 null :: Queue a #-> ((Bool, Queue a) #-> b) #-> b
-null (Queue l m) f = f (P.null l, Queue l m)
+null (Queue l m) k = k (P.null l, Queue l m)
 
 enqueue :: a -> Queue a #-> (Queue a #-> b) #-> b
-enqueue a (Queue l m) f = f (check l (a:m))
+enqueue a (Queue l m) k = k (check l (a:m))
 
 dequeue :: Queue a #-> (Maybe (a, Queue a) #-> b) #-> b
-dequeue (Queue (a:l) m) f = f (Just (a, check l m))
-dequeue (Queue _ _) f = f Nothing
+dequeue (Queue (a:l) m) k = k (Just (a, check l m))
+dequeue (Queue _ _) k = k Nothing
 
 check :: [a] -> [a] -> Queue a
 check [] m = Queue (reverse m) []
